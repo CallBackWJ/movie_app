15e1ab0eaf78d4e33d771ee74d6e3b7c5d2d1861
choi <choi@choiui-MacBook-Pro.local>
2019-04-15 13:02:19 +0900

    add

---
+++ src/Movie.js
@@ -0,0 +1,20 @@
+import React,{ Component } from 'react';
+import './movie.css';
+
+
+
+class Movie extends Component{
+
+    render(){
+        return (
+
+            <div className='Movie'>
+            <h1>극한직업</h1>
+             <img src='http://www.topdigital.com.au/sites/default/files/극한직업222.jpg'/>
+        </div>
+
+        );
+    }
+}
+
+export default Movie;
